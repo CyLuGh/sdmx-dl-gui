04a2a1c2505984d72f197cbdcca4e203077bc39e
Jeremy Demortier <64773074+CyLuGh@users.noreply.github.com>
2025-04-07 11:21:07 +0200

    Add About

--- src/SdmxDl.Browser/MainWindow.axaml.cs
+++ src/SdmxDl.Browser/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Jot;
 using Splat;
 using SukiUI.Controls;
@@ -16,5 +19,26 @@ public partial class MainWindow : SukiWindow
     {
         var tracker = Locator.Current.GetService<Tracker>();
         tracker?.PersistAll();
+    }
+
+    private void GitHubLinkClick(object? sender, RoutedEventArgs e)
+    {
+        var url = (e.Source as HyperlinkButton).Content as string;
+
+        if (string.IsNullOrEmpty(url))
+            return;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
     }
 }
